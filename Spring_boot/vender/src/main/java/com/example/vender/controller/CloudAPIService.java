8149fa5c52ec179976c4a7923647a3fa7c747bcd
AnantSagar01 <anantsagar0069@gmail.com>
2025-03-17 03:48:05 +0530

    added

--- Spring_boot/vender/src/main/java/com/example/vender/controller/CloudAPIService.java
+++ Spring_boot/vender/src/main/java/com/example/vender/controller/CloudAPIService.java
@@ -1,7 +1,10 @@
 package com.example.vender.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,30 +12,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.vender.model.CloudVendor;
+import com.example.vender.service.CloudVendorService;
 
 @RestController
 @RequestMapping("/cloudvendor")
 public class CloudAPIService {
-    CloudVendor cloudVendor;
+    CloudVendorService cloudVendorService;
+
+    public CloudAPIService(CloudVendorService cloudVendorService) {
+        this.cloudVendorService = cloudVendorService;
+    }
 
     @GetMapping("{vendorId}")
-    public CloudVendor getCloudVendorDetails(String vendorId){
+    public CloudVendor getCloudVendorDetails(@PathVariable("vendorId") String vendorId){
         // return new CloudVendor("C1","Vendor 1","Bangalore","8877474528");
-        return cloudVendor;
+        
+        return cloudVendorService.getcloudVendor(vendorId);
+    }
+    @GetMapping()
+    public List<CloudVendor> getAllCloudVendorDetails(){
+        // return new CloudVendor("C1","Vendor 1","Bangalore","8877474528");
+    
+        return cloudVendorService.getAllCloudVendors();
     }
     @PostMapping
     public String createCloudVanderDetails(@RequestBody CloudVendor cloudVendor){
-        this.cloudVendor = cloudVendor;
+        cloudVendorService.createCloudVendor(cloudVendor);
         return "Cloud Vandor Created";
     }
     @PutMapping
     public String updateCloudVanderDetails(@RequestBody CloudVendor cloudVendor){
-        this.cloudVendor = cloudVendor;
+        cloudVendorService.updateCloudVendor(cloudVendor);
         return "Cloud Vandor Updated";
     }
     @DeleteMapping("{vendorId}")
-    public String deleteCloudVanderDetails(String vendorId){
-        this.cloudVendor = null;
+    public String deleteCloudVanderDetails(@PathVariable("vendorId") String vendorId){
+        cloudVendorService.deleteCloudVendor(vendorId);
         return "Cloud Vandor deleted";
     }
 
--- Spring_boot/vender/src/main/java/com/example/vender/service/CloudVendorService.java
+++ Spring_boot/vender/src/main/java/com/example/vender/service/CloudVendorService.java
@@ -1,5 +1,13 @@
 package com.example.vender.service;
 
-public interface CloudVendorService {
+import java.util.List;
 
+import com.example.vender.model.CloudVendor;
+
+public interface CloudVendorService {
+    public String createCloudVendor(CloudVendor cloudVendor);
+    public String updateCloudVendor(CloudVendor cloudVendor);
+    public String deleteCloudVendor(String cloudVendorId);
+    public CloudVendor getcloudVendor(String cloudVendorId);
+    public List<CloudVendor> getAllCloudVendors();
 }
